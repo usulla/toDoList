eb04f1ee8306a97298b06c649edc1e69db65b415
Maria <maria_suxareva@mail.ru>
2020-03-22 21:25:01 +0300

    Added field for add item

---
+++ src/TodoList/TodoList.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import Form from 'react-bootstrap/Form';
+import FormGroup from 'react-bootstrap/FormGroup';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import TodoItems from '../TodoItems/TodoItems';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+    this.addItem = this.addItem.bind(this);
+  }
+  addItem(e) {
+    if (this._inputElement.value !== "") {
+      var newItem = {
+        text: this._inputElement.value,
+        key: Date.now()
+      }
+
+      this.setState((prevState => {
+        return {
+          items: prevState.items.concat(newItem)
+        };
+      }));
+
+      this._inputElement.value = "";
+    }
+    e.preventDefault();
+
+    setTimeout(() => {
+      console.log(this.state.items)
+    }, 0);
+  }
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <Form onSubmit={this.addItem}>
+            <FormGroup controlId="formAddItem">
+              <InputGroup className="mb-3">
+                <FormControl
+                  ref={(a) => this._inputElement = a}
+                  placeholder="enter task"
+                />
+                <InputGroup.Append>
+                  <Button type="submit" variant="primary">Add</Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </FormGroup>
+          </Form>
+        </div>
+        <TodoItems entries={this.state.items}/>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
