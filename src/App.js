4d281463dd3ca5ae3511f3c2081de760313ade28
Maria <maria_suxareva@mail.ru>
2020-03-22 21:24:24 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components'
 import './App.css';
+import TodoList from './TodoList/TodoList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Title = styled.h1`
+  color:#000000;
+`;
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items:center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+        <Title>Hello!</Title>
+        <TodoList/>
+    </Wrapper>
   );
 }
 
